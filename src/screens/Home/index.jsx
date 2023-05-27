9823b4d2882c56664867cf1dcd188499a761b081
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-05-27 13:54:17 -0500

    edited

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,13 +1,51 @@
 import {SearchBar} from '@rneui/themed';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import {getTenCharacters} from '../../api/characters';
+import CharactersList from './CharacterLists/CharactersList';
+
 import {View} from 'react-native';
-import Card from '../../components/Card';
-import LogoImage from '../../components/Logo';
 import styles from './styles';
 const HomeScreen = () => {
   const [search, setSearch] = useState('');
 
+  const [characters, setCharacters] = useState([]);
+
+  const [offset, setOffset] = useState(0);
+
   //useEffect to send info to the Card components
+
+  useEffect(() => {
+    (async () => {
+      await loadCharacters();
+    })();
+  }, []);
+
+  const loadCharacters = async () => {
+    try {
+      const response = await getTenCharacters(offset);
+      setOffset(offset + 10);
+      const charactersArray = [];
+      for await (const character of response.data.results) {
+        charactersArray.push({
+          id: character.id,
+          name: character.name,
+          description: character.description,
+          image: character.thumbnail.path + '.' + character.thumbnail.extension,
+          totalComics: character.comics.available,
+          totalSeries: character.series.available,
+          totalEvents: character.events.available,
+          dateModified: character.modified,
+        });
+      }
+
+      setCharacters([...characters, ...charactersArray]);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
+
+  if (characters === null) return null;
 
   return (
     <View style={styles.container}>
@@ -19,22 +57,26 @@ const HomeScreen = () => {
         containerStyle={{
           backgroundColor: '#ED1D24',
           marginHorizontal: 25,
-          marginTop: 20,
+          marginTop: 10,
         }}
         inputContainerStyle={{
           backgroundColor: 'white',
           borderRadius: 20,
         }}
         labelStyle={{color: 'red', backgroundColor: 'white'}}
-        inputStyle={{color: 'red', backgroundColor: 'white', borderRadius: 20}}
+        inputStyle={{
+          color: 'red',
+          backgroundColor: 'white',
+          borderRadius: 20,
+        }}
         cancelButtonProps={{color: 'white'}}
       />
-      <LogoImage />
 
-      {/*
-      FlatList
-      */}
-      <Card />
+      <CharactersList
+        characters={characters}
+        offset={offset}
+        loadCharacters={loadCharacters}
+      />
     </View>
   );
 };
