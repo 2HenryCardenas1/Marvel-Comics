f194b87764d12f41f5c29960f13772f1662cdbe0
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-06-13 14:32:36 -0500

    Context implemented

--- App.tsx
+++ App.tsx
@@ -11,6 +11,7 @@ import {StatusBar} from 'react-native';
 import {Amplify} from 'aws-amplify';
 import 'react-native-gesture-handler';
 import config from './src/aws-exports';
+import {AuthProvider} from './src/context/AuthContext';
 import Navigation from './src/navigation/Navigation';
 
 Amplify.configure(config);
@@ -19,7 +20,10 @@ function App(): JSX.Element {
   return (
     <>
       <StatusBar barStyle={'dark-content'} backgroundColor={'#fff'} />
-      <Navigation />
+
+      <AuthProvider>
+        <Navigation />
+      </AuthProvider>
     </>
   );
 }
