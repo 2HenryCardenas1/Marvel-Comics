fccb9edceb1d1ae4f407cca0e3b7a7d47d5bac80
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-06-13 14:33:53 -0500

    no logged created

---
+++ src/screens/NoLogged/NoLogged.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const NoLogged = () => {
+  return (
+    <View>
+      <Text>NoLogged</Text>
+    </View>
+  )
+}
+
+export default NoLogged
