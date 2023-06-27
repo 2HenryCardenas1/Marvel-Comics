08a054fd704b3cfc450b99aec353ba66ed6c325a
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-06-27 13:01:48 -0500

    profile screen edited

--- src/screens/Profile/profile.jsx
+++ src/screens/Profile/profile.jsx
@@ -1,11 +1,12 @@
-import {Button} from '@rneui/themed';
+import {Button, Divider} from '@rneui/themed';
 import React from 'react';
 import {ActivityIndicator, Text, View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 import useAuth from '../../hooks/useAuth';
 
 const Profile = () => {
   const {logout, attributes} = useAuth();
-  console.log('Attributes: ', attributes);
+
   if (attributes === undefined || attributes === null) {
     return (
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
@@ -13,11 +14,81 @@ const Profile = () => {
       </View>
     );
   }
+
   return (
-    <View>
-      <Text>profile, {attributes.email}</Text>
-      <Button title={'Logout'} onPress={() => logout()} />
-    </View>
+    <SafeAreaView
+      style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <View
+        style={{
+          width: '90%',
+          marginTop: 20,
+          marginBottom: 20,
+          borderWidth: 0.01,
+          shadowColor: 'black',
+          shadowOpacity: 0.2,
+          elevation: 1,
+          shadowRadius: 5,
+          padding: 10,
+        }}>
+        {cardData(
+          attributes.name + ' ' + attributes.family_name,
+          (title = 'Name'),
+          (index = 0),
+        )}
+        {cardData(attributes.email, (title = 'Email'))}
+        {cardData(attributes.preferred_username, (title = 'Username'))}
+        {cardData(0, (title = 'Total Favorites'))}
+      </View>
+      <Button title={'Logout'} onPress={() => logout()} color={'#ED1D24'} />
+    </SafeAreaView>
+  );
+};
+
+const cardData = (data, title, index) => {
+  return (
+    <>
+      {index === 0 ? null : (
+        <Divider
+          style={{
+            marginVertical: 10,
+          }}
+        />
+      )}
+
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          marginHorizontal: 10,
+        }}>
+        <View>
+          <Text
+            style={{
+              fontSize: 18,
+              fontWeight: 'bold',
+              color: 'black',
+            }}>
+            {title} :
+          </Text>
+        </View>
+
+        <View
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text
+            style={{
+              fontSize: 16,
+            }}>
+            {data}
+          </Text>
+        </View>
+      </View>
+    </>
   );
 };
 
