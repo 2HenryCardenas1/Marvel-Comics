be447ba3b22b58366d86443c8358e69a6d36fbf7
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-07-12 13:37:11 -0500

    go to top implemented

--- src/screens/Home/CharacterLists/CharactersList.jsx
+++ src/screens/Home/CharacterLists/CharactersList.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import {Icon} from '@rneui/themed';
+import React, {useRef} from 'react';
 import {
   ActivityIndicator,
   FlatList,
@@ -6,52 +7,79 @@ import {
   Text,
   View,
 } from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 import Card from '../../../components/Card';
 import LogoImage from '../../../components/Logo';
 
 const CharactersList = props => {
   const {characters, offset, loadCharacters, loading, screen} = props;
 
+  const flatListRef = useRef(null);
+
   const loadMore = () => {
     loadCharacters(offset);
   };
 
+  const goToTop = () => {
+    if (characters.length > 10) {
+      return (
+        <View style={{position: 'absolute', bottom: 20, right: 20}}>
+          <TouchableOpacity
+            onPress={() =>
+              flatListRef.current.scrollToOffset({animated: true, offset: 0})
+            }>
+            <Icon
+              type="material-community"
+              name="arrow-up-circle"
+              size={30}
+              color="#ED1D24"
+            />
+          </TouchableOpacity>
+        </View>
+      );
+    }
+  };
+
   return (
-    <FlatList
-      data={characters}
-      key={item => item.id}
-      keyExtractor={item => item.id.toString()}
-      ListHeaderComponent={() => (
-        <>
-          {screen === 'Results' ? null : <LogoImage />}
-          {loading && (
-            <View>
-              <ActivityIndicator size="large" color="#fefefe" />
-              <Text
-                style={{
-                  textAlign: 'center',
-                  fontSize: 16,
-                  color: '#fefefe',
-                }}>
-                Loading characters ...
-              </Text>
-            </View>
-          )}
-        </>
-      )}
-      renderItem={({item}) => <Card character={item} />}
-      onEndReached={screen === 'Results' ? null : loadMore}
-      onEndReachedThreshold={0.1}
-      ListFooterComponent={
-        offset && (
-          <ActivityIndicator
-            size="large"
-            style={styles.spinner}
-            color="#aeaeae"
-          />
-        )
-      }
-    />
+    <>
+      <FlatList
+        ref={flatListRef}
+        data={characters}
+        key={item => item.id}
+        keyExtractor={item => item.id.toString()}
+        ListHeaderComponent={() => (
+          <>
+            {screen === 'Results' ? null : <LogoImage />}
+            {loading && (
+              <View>
+                <ActivityIndicator size="large" color="#fefefe" />
+                <Text
+                  style={{
+                    textAlign: 'center',
+                    fontSize: 16,
+                    color: '#fefefe',
+                  }}>
+                  Loading characters ...
+                </Text>
+              </View>
+            )}
+          </>
+        )}
+        renderItem={({item}) => <Card character={item} />}
+        onEndReached={screen === 'Results' ? null : loadMore}
+        onEndReachedThreshold={0.1}
+        ListFooterComponent={
+          offset && (
+            <ActivityIndicator
+              size="large"
+              style={styles.spinner}
+              color="#aeaeae"
+            />
+          )
+        }
+      />
+      {goToTop()}
+    </>
   );
 };
 
